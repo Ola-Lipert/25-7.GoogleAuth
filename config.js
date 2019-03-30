e504ebfd3fdafba0145138341c97cf3a6ce74c45
Aleksandra Lipert <ola.luszcz@interia.pl>
2019-03-31 00:33:26 +0100

    Add task 25.7

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '1084536299067-o6tkvd6ohe475cra9k5jvq996h0g4bl5.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'zJBCyQ4-rQ9aNxNNj50QR89Z',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
